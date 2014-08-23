f0a5a740a7f743fe02a15890bb28bb73eb1cedf3
lk <lightning-li>
2014-08-23 22:31:30 +0800

    head file for list_queue

---
+++ queue.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: queue.h
+	> Author:likang 
+	> Mail: 
+	> Created Time: 2014年08月23日 星期六 21时13分48秒
+ ************************************************************************/
+
+void insert(int value);
+void delete(void);
+int first(void);
+int is_empty(void);
+int is_full(void);
+void destroy_queue(void);
