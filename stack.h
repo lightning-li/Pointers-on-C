28386e1b585eea5230e685a9b38ee592175ffad8
lk <lightning-li>
2014-08-23 22:30:33 +0800

    head file for list_stack

---
+++ stack.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: stack.h
+	> Author:likang 
+	> Mail: 
+	> Created Time: 2014年08月23日 星期六 20时21分31秒
+ ************************************************************************/
+
+void push(int value);
+void pop(void);
+int top(void);
+int is_empty(void);
+int is_full(void);
+void destory(void);
